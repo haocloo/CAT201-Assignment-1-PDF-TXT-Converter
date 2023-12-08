3fa9690a895f0849047abb2d73bed3010b55f09c
Chi Hao <81066169+haocloo@users.noreply.github.com>
2023-12-08 14:17:50 +0800

    Getting ready for recording

--- php/index.php
+++ php/index.php
@@ -9,7 +9,7 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Convert PDF to TXT and TXT to PDF easily without installing any software.">
   <title>PDF and TXT Converter</title>
-  <link rel="stylesheet" type="text/css" href="../css/output.css">
+  <link rel="stylesheet" type="text/css" href="../css/index.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
